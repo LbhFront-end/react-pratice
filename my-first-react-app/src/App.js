959ba8c0109d37ab380754c4c1baa2951a77948b
Lbh <544289495@qq.com>
2018-06-07 09:50:12 +0800

    The Road to React Part2 Practice

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -16,48 +16,88 @@ const list = [{
   author: 'Dan Abramov, Andrew Clark',
   num_comments: 2,
   points: 5,
-  objectID: 1,  
+  objectID: 1,
 }];
 
 
+class Button extends Component {
+  render() {
+    const { onClick, className, children } = this.props;
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button">
+        {children}
+      </button>
+    )
+  }
+}
+
 const isSearched = searchText => item => item.title.toLowerCase().includes(searchText.toLowerCase());
-class FormP extends Component{
-  constructor(props){
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children}
+    <input
+      type="text"
+      value={value}
+      onChange={onChange} />
+  </form>
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+          <Button className="button-inline"
+            onClick={() => onDismiss(item.objectID)}>
+            Dismiss
+        </Button>
+        </span>
+      </div>
+    )}
+  </div>
+class FormP extends Component {
+
+  constructor(props) {
     super(props);
-    this.state = {list,searchText:''};
+    this.state = { list, searchText: '' };
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
-  onSearchChange(e){
-    this.setState({searchText:e.target.value});
+
+  onSearchChange(e) {
+    this.setState({ searchText: e.target.value });
   }
-  onDismiss(id){
+
+  onDismiss(id) {
     console.log(this);
     const updateList = this.state.list.filter(item =>
       item.objectID !== id
     );
-    this.setState({list:updateList});
+    this.setState({ list: updateList });
   }
-  render(){
-    return(
-      <div className = "FormP">
-        <form>
-          <input type="text" onChange = {this.onSearchChange}/>
-        </form>
-        {this.state.list.filter(isSearched(this.state.searchText)).map(
-          item =>
-          <div key = {item.objectID}>
-          <span>
-            <a href= {item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button onClick = {()=>this.onDismiss(item.objectID)}>Dismiss</button>
-          </span>
-        </div>  
-        )}
+
+  render() {
+    const { searchText, list } = this.state;
+    return (
+      <div className="page">
+        <div className = "interactions">
+          <Search
+            value={searchText}
+            onChange={this.onSearchChange} />
+          <Table
+            list={list}
+            pattern={searchText}
+            onDismiss={this.onDismiss} />
+        </div>
       </div>
     )
   }
